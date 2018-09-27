a1f87d5cfb54cd24cdff16e46ef729ab61701f99
Rafael Fronteira <rafael_fronteira@hotmail.com>
2018-09-27 08:34:33 -0300

    adjusting code style

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const bodyParser = require('body-parser');
 const routes = require('./api/routes')
 const config = require('./config').get();
 const mongoose = require('mongoose');
-mongoose.connect(config.mongodb.url);
 const port = process.env.PORT || 3000;
-mongoose.Promise = global.Promise;
 const environment = process.env.NODE_ENV || 'development';
 
+
 const app = express();
+
+// Mongodb configuration
+mongoose.connect(config.mongodb.url);
+mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 routes.set(app);
 
 app.listen(port, () => {
@@ -21,6 +27,7 @@ app.listen(port, () => {
 });
 
 db.on('error', console.error.bind(console, 'connection: error'));
-db.once('open', function() {
+
+db.once('open', () => {
     console.log(`Conectado no banco de dados: ${environment}`);
 });
