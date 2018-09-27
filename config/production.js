faa35c576f1bea2fc5bb2a57817f817c8d27c8cc
RafaelFronteira <rafael_fronteira@hotmail.com>
2018-09-26 23:39:02 -0300

    mongodb connection done

--- config/production.js
+++ config/production.js
@@ -1,5 +1,7 @@
 const production = {
-
+    mongodb: {
+        "url": 'mongodb://mypoopy:mypoopy762@ds115753.mlab.com:15753/mypoopydb'
+    }
 };
 
 module.exports = production;
--- server.js
+++ server.js
@@ -2,15 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./api/routes')
 const config = require('./config').get();
+const mongoose = require('mongoose');
+mongoose.connect(config.mongodb.url);
 const port = process.env.PORT || 3000;
+mongoose.Promise = global.Promise;
 const environment = process.env.NODE_ENV || 'development';
 
 const app = express();
+const db = mongoose.connection;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 routes.set(app);
 
 app.listen(port, () => {
     console.log(`Ouvindo na porta http://locahost:${port}`);
     console.log(`Ambiente: ${environment}`);
+});
+
+db.on('error', console.error.bind(console, 'connection: error'));
+db.once('open', function() {
+    console.log(`Conectado no banco de dados: ${environment}`);
 });
