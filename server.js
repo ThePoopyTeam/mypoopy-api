0d59106418cb6dd857e7973a90d32a192ba668b4
Rafael Fronteira <rafael_fronteira@hotmail.com>
2018-10-16 13:54:43 -0300

    corrindo erro de merge

--- server.js
+++ server.js
@@ -3,24 +3,27 @@ const bodyParser = require('body-parser');
 const routes = require('./api/routes')
 const config = require('./config').get();
 const mongoose = require('mongoose');
-mongoose.connect(config.mongodb.url, { useNewUrlParser: true });
 const port = process.env.PORT || 3000;
 const environment = process.env.NODE_ENV || 'development';
+
+// firebase configurations
 const admin = require('firebase-admin');
 const serviceAccount = require('./config/mypoopyproject-198f6-firebase-adminsdk-66njv-40a63e4c91.json');
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://mypoopyproject-198f6.firebaseio.com"
 });
 
-const app = express();
 
-// Mongodb configuration
-mongoose.connect(config.mongodb.url);
+
+// Mongodb configurations
+mongoose.connect(config.mongodb.url, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
+
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
