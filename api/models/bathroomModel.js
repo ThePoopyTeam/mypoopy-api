0686a577ddb3233744c05dea5409dfb28bb17c24
RAFAEL SANTOS FRONTEIRA <RAFAEL SANTOS FRONTEIRA>
2018-11-20 20:35:14 -0200

    uid referenciado no banheiro

--- api/models/bathroomModel.js
+++ api/models/bathroomModel.js
@@ -2,6 +2,7 @@ const restful = require("node-restful");
 const mongoose = restful.mongoose;
 
 const BathroomSchema = new mongoose.Schema({
+    uid: { type: String, required: true },
     nome: { type: String, trim: true, required: true, max: 50 },
     endereco: { type: String, trim: true, required: true, max: 150 },
     caracte: [{ nome: String, icone: String }],
